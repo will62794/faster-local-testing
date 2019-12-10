6e5fb7dd8ba99a4935ab8188ac62a8294db61559
William Schultz <william.schultz@mongodb.com>
2019-12-10 12:14:16 -0500

    Add extra sharding metric

--- statssharding.sh
+++ statssharding.sh
@@ -14,6 +14,7 @@ echo "Processing $i"
 # We also add the metadata columns which are contained in the filename.
 grep "ShardingTest.*took" $logdir/$i \
 | sed -E -e "s/\[.*:(.*)\].*startup and initiation for all nodes.*took (.*)ms with (.*) config server nodes and (.*) total shard nodes/$i,\1,startupAndInitiate,\2,\4/" \
+         -e "s/\[.*:(.*)\].*startup for all nodes took (.*)ms with (.*) config server nodes and (.*) total shard nodes/$i,\1,startShardsAndConfigServer,\2,\4/" \
          -e "s/\[.*:(.*)\].*stopped all.*took (.*)ms for (.*) shards.*/$i,\1,stopShards,\2,\3/" \
          | grep -v "js_test:" >> $csv # remove any unprocessed lines.
 done
