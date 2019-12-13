9f04dda52d08899864025bdf1e2e86505c68bc92
William Schultz <william.schultz@mongodb.com>
2019-12-13 17:47:43 -0500

    Remove old scripts

--- old/allhist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Generate CSV duration data from each log for replica sets and sharding suites.
-for i in `ls logs | grep -v "csv" | grep "replica_sets"`; do ./statsrepl.sh $i; done
-for i in `ls logs | grep -v "csv" | grep "sharding"`; do ./statssharding.sh $i; done
-
-# Generate histogram charts for all logs.
-python hist.py `ls logs | grep -v "csv" | grep "replica_sets" | paste -s -d, -` startSet,initiateWithNodeZeroAsPrimary,stopSetShutdown,stopSetConsistencyChecks "charts/durations_replica_sets.svg"
-python hist.py `ls logs | grep -v "csv" | grep "sharding" | paste -s -d, -` startupAndInitiate,startupAndInitiateConfigServer,stopShards "charts/durations_sharding.svg"
--- old/download.sh
+++
@@ -1,5 +0,0 @@
-# Download each log file.
-echo "" > all_logs.txt
-for url in `cat urls.txt`; 
-	do curl "$url" >> all_logs.txt
-done
