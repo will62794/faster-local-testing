aff227fdfb3de447a0acde4bc8c0d8c197044ee8
William Schultz <william.schultz@mongodb.com>
2020-04-09 15:57:58 -0400

    Revert "Merge pull request #1 from ajdavis/get-variant-logs"
    This reverts commit ad99376f89dceea5ad79c01301e2027a7d5cf720, reversing
    changes made to 653f2b29bf95bf496437d3714e5f0623ca1041eb.

--- getlogs.py
+++ getlogs.py
@@ -1,17 +1,18 @@
-"""
-Fetch logs of a given Evergreen patch/version build, or one task within a build.
-"""
-
-import argparse
-import os
-import re
+#
+# Fetch log URLs for all logs of a given Evergreen task.
+#
 
 import requests
-from dateutil.parser import parse as dateutil_parse
+import time
+import argparse
+import sys
+import pprint
+import urllib2
+import os
+from datetime import datetime
+import time
 
 # MongoDB 'master' branch project identifier in Evergreen.
-import tqdm
-
 MONGO_PROJECT = "mongodb-mongo-master"
 
 # The Evergreen REST API endpoint.
@@ -26,25 +27,14 @@ API_KEY = "" # must be set via command line.
 def header():
     return {"Api-User": API_USER, "Api-Key": API_KEY}
 
-def _get_resource(resource_type, resource_id):
-    # Note the "s": we get a version from "/versions/".
-    url = API_URL_V1 + "/" + resource_type + "s/" + resource_id
-    r = requests.get(url=url, headers=header())
-    return r.json()
-
-def get_version(version_id):
-    return _get_resource('version', version_id)
-
-def get_build(build_id):
-    return _get_resource('build', build_id)
-
-def get_task(task_id):
-    return _get_resource('task', task_id)
-
 def get_tests(task_id, limit):
     url_task_tests = API_URL + "/tasks/" + task_id + "/tests"
-    r = requests.get(url=url_task_tests, params={"limit": limit},
-                     headers=header())
+    r = requests.get(url=url_task_tests, params={"limit": limit}, headers=header())
+    return r.json()
+
+def get_task(task_id):
+    url_task = API_URL_V1 + "/tasks/" + task_id
+    r = requests.get(url=url_task, headers=header())
     return r.json()
 
 def get_log(url):
@@ -55,8 +45,8 @@ def save_test_durations(tests, file_prefix):
     durations = []
     for t in tests:
         test_name = t['test_file'].split("/")[-1].split(".")[0]
-        startt = float(dateutil_parse(t['start_time']).strftime('%s.%f'))
-        endt = float(dateutil_parse(t['end_time']).strftime('%s.%f'))
+        startt = float(datetime.strptime(t['start_time'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%s.%f'))
+        endt = float(datetime.strptime(t['end_time'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%s.%f'))
         duration_ms = (endt-startt)*1000
         duration = [test_name, duration_ms]
         durations.append(duration)
@@ -67,17 +57,6 @@ def save_test_durations(tests, file_prefix):
         row = ",".join([str(el) for el in d])
         f.write(row + "\n")
     return durations
-
-def download_file(url, filename):
-    r = requests.get(url, stream=True)
-    chunk_size = 1024
-
-    with open(filename, 'wb') as fp:
-        for chunk in tqdm.tqdm(
-                r.iter_content(chunk_size=chunk_size),
-                unit='KB',
-                desc=filename):
-            fp.write(chunk)
 
 def check_task(task_id, durations_only):
     """ Get all log files for a given task id. """
@@ -99,16 +78,14 @@ def check_task(task_id, durations_only):
 
     # Get all tests from this task.
     tests = get_tests(task_id, 10000)
-    if type(tests) == dict and tests.get('status') != 200:
-        print 'Error', tests['status'], tests.get('error')
-        return
-
     test_log_urls = [test['logs']['url_raw'] for test in tests]
     job_log_urls = list(
         set([u[:u.find("/test/")] + "/all?raw=1" for u in test_log_urls]))
     print str(len(tests)) + " tests for this task.", str(len(job_log_urls)) + " job log URLs."
 
     # Save the log files into one big file.
+    out_file = logs_dir + "/" + task_id
+
     # Include identifying information in the log file name.
     elems = [task["build_variant"], task["display_name"], patch_id, task["revision"]]
     out_file_name = ",".join(elems)
@@ -120,75 +97,23 @@ def check_task(task_id, durations_only):
     if durations_only:
         return
 
+    f = open(out_file, "w")
     for url in job_log_urls:
         print "Downloading log file: " + url
-        max_tries = 10
-        while True:
-            try:
-                download_file(url, out_file)
-                break
-            except Exception as exc:
-                if max_tries:
-                    print repr(exc), "Retrying..."
-                    max_tries -= 1
-                else:
-                    print repr(exc), "Giving up"
-
-def check_version(version_id, durations_only, task_pattern):
-    version = get_version(version_id)
-
-    print "Version ID:", version_id
-    print len(version['builds']), "builds"
-
-    builds = [get_build(build_id) for build_id in version['builds']]
-    tasks = [task for build in builds for task in build['tasks'].values()]
-    print len(tasks), "tasks"
-
-    for i, task in enumerate(tasks):
-        task_id = task['task_id']
-        if task['status'] == 'failed':
-            continue
-
-        if task_pattern and not task_pattern.match(task_id):
-            continue
-
-        print "Task ID: %s, %d of %d" % (task_id, i, len(tasks))
-        check_task(task_id, durations_only)
+        text = urllib2.urlopen(url).read()
+        f.write(text)
+    f.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument('API_USER',
-                        help='Evergreen username',
-                        nargs=1)
-    parser.add_argument('API_KEY',
-                        help='Evergreen API key',
-                        nargs=1)
-    parser.add_argument('--task',
-                        help='Evergreen task ID')
-    parser.add_argument('--task-pattern',
-                        help='Python regex to match task names within a build',
-                        type=re.compile)
-    parser.add_argument('--version',
-                        help='Evergreen version ID or patch ID')
-    parser.add_argument('--durations-only',
-                        help='Measure durations without fetching logs',
-                        action='store_true')
-
-    args = parser.parse_args()
-
-    API_USER = args.API_USER[0]
-    API_KEY = args.API_KEY[0]
-    if not args.task and not args.version:
-        parser.error('Must supply --task or --version')
-    elif args.task and args.version:
-        parser.error('Cannot supply both --task and --version')
-    elif args.task_pattern and not args.version:
-        parser.error('Cannot supply both --task and task-pattern')
-
+    # Must provide Evergreen credentials via command line.
+    API_USER = sys.argv[1]
+    API_KEY = sys.argv[2]
+    task = sys.argv[3]
+    durations_only = False
+    if len(sys.argv) > 4:
+        durations_only = (sys.argv[4] == "durations")
+    
     # Create output directory if it doesn't exist.
     os.system("mkdir -p logs")
-
-    if args.task:
-        check_task(args.task, args.durations_only)
-    else:
-        check_version(args.version, args.durations_only, args.task_pattern)
+    
+    check_task(task, durations_only)
